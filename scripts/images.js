8e04488fe5869aaa832e0d416c303371f71c3f35
Ben Hoskings <ben@hoskings.net>
2010-09-03 14:14:30 +0900

    Working on image resizing algorithm.

--- scripts/images.js
+++ scripts/images.js
@@ -1,23 +1,43 @@
-var positionLightbox = function() {
-  var padding = 10,
-      img = $('.lightbox img'),
-      pagewidth = $(window).width() - padding - padding,
-      pageheight = $(window).height() - padding - padding,
-      width = Math.min(pagewidth, img.width()),
-      height = Math.min(pageheight, img.height());
-
-  if (width / height > pagewidth / pageheight) {
-    img.css({
-      width: pagewidth
-    });
-  } else {
-    img.css({
-      width: pagewidth
+var setNativeDimensions = function(img) {
+  if (!img.data('dimensions')) {
+    offscreenImage = new Image();
+    offscreenImage.src = img.attr('src');
+    img.data('dimensions', {
+      'width': offscreenImage.width,
+      'height': offscreenImage.height
     });
   }
+  return img.data('dimensions');
+};
+
+var positionLightbox = function() {
+  var padding = 12; // $body_padding from styles.sass
+  var minWidth = 640; // $width from styles.sass
+  var img = $('.lightbox img');
+  var dims = setNativeDimensions(img);
+  var pagewidth = $(window).width() - padding - padding;
+  var pageheight = $(window).height() - padding - padding;
+  var width = Math.max(minWidth, Math.min(pagewidth, dims.width));
+  console.log("var width = Math.max(" + minWidth + ", Math.min(" + pagewidth + ", " + dims.width + "));")
+  var height = Math.max(
+    (minWidth * dims.height / width),
+    Math.min(pageheight, dims.height)
+  );
+
+  console.log("if (" + width + " / " + height + " > " + pagewidth + " / " + pageheight + ")");
+  if (width / height > pagewidth / pageheight) {
+    console.log("img.css({width: " + width + "});");
+    img.css({
+      'width': width
+    });
+  } else {
+    // img.css({
+    //   width: pagewidth
+    // });
+  }
   img.css({
-    top: ((padding + (pageheight - height) / 2) + 'px'),
-    left: ((padding + (pagewidth - width) / 2) + 'px')
+    top: (Math.max(padding, (padding + (pageheight - height) / 2)) + 'px'),
+    left: (Math.max(padding, (padding + (pagewidth - width) / 2)) + 'px')
   });
 };
 var showFullSizeImage = function(uri) {

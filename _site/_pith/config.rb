73f02c1c8f75fa2cdd26267752da063c206c1060
Ben Hoskings <ben@hoskings.net>
2012-03-02 19:58:44 +1100

    Well that was easy.

--- _site/_pith/config.rb
+++ _site/_pith/config.rb
@@ -1,2 +1,14 @@
 project.assume_content_negotiation = true
 project.assume_directory_index = true
+
+project.helpers do
+  def posts
+    project.inputs.select {|input|
+      input.path.basename.to_s[/^\d{4}\-\d{2}\-\d{2}/]
+    }
+  end
+
+  def date_of post
+    Time.new *post.path.basename.to_s.scan(/^\d{4}\-\d{2}\-\d{2}/).first.split('-')
+  end
+end

5c17239df56636fc2abee5a1f19f926ea71bc3af
Ben Hoskings <ben@hoskings.net>
2012-03-02 20:22:17 +1100

    Use date heirachy on disk to preserve paths.

--- site/_pith/config.rb
+++ site/_pith/config.rb
@@ -4,11 +4,11 @@ project.assume_directory_index = true
 project.helpers do
   def posts
     project.inputs.select {|input|
-      input.path.basename.to_s[/^\d{4}\-\d{2}\-\d{2}/]
+      input.path.dirname.to_s[%r{^\d{4}/\d{2}/\d{2}}]
     }
   end
 
   def date_of post
-    Time.new *post.path.basename.to_s.scan(/^\d{4}\-\d{2}\-\d{2}/).first.split('-')
+    Time.new *post.path.dirname.to_s.split('/')
   end
 end

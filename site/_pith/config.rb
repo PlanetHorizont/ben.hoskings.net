2d2d511bf005af7d9b4473996eea6b90537fc628
Ben Hoskings <ben@hoskings.net>
2012-05-21 00:58:59 +1000

    Don't break between the last two words in a headline (via TC).

--- site/_pith/config.rb
+++ site/_pith/config.rb
@@ -15,4 +15,13 @@ project.helpers do
   def slug_for page
     page.title.gsub(/\W+/, '-')
   end
+
+  def formatted_headline_for text
+    # Make the final space non-breaking if the final two words fit within 20 characters.
+    if text.length > 20 && text[-20..-1][/\s+\S+\s+\S+$/].nil?
+      text
+    else
+      text.gsub /\s+(?=\S+$)/, "&nbsp;"
+    end
+  end
 end
